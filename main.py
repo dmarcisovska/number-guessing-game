944349be2e68e14c85a9002c814326f25aea64bb
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 18:29:18 -0700

    added logic if user enters no input

--- main.py
+++ main.py
@@ -2,18 +2,22 @@ import random
 keep_playing = True
 
 while keep_playing:
+
     ran_num = random.randint(1, 10)
-    guess = int(input("Guess a number between 1 and 10: "))
-    if ran_num > guess:
-        print("Too low!")
-    elif ran_num < guess:
-        print("Too high!")
-    elif guess == ran_num:
-        print("YOU GOT IT!!")
-        want_continue = input("Do you want to keep playing? y/n ")
-        if want_continue == "y":
-            keep_playing = True
-        else:
-            keep_playing = False
+    guess = input("Guess a number between 1 and 10: ")
+    if guess:
+        guess = int(guess)
+        if ran_num > guess:
+            print("Too low!")
+        elif ran_num < guess:
+            print("Too high!")
+        elif guess == ran_num:
+            print("YOU GOT IT!!")
+            want_continue = input("Do you want to keep playing? y/n ")
+            if want_continue == "y":
+                keep_playing = True
+            else:
+                keep_playing = False
     else:
-        print("something went wrong")
+        print("Something went wrong!")
+
