ed74c955366e267609b91f0f090ae8b259f051ac
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 18:27:21 -0700

    added print statements for too high and too low numbers

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ keep_playing = True
 while keep_playing:
     ran_num = random.randint(1, 10)
     guess = int(input("Guess a number between 1 and 10: "))
-    if guess == ran_num:
+    if ran_num > guess:
+        print("Too low!")
+    elif ran_num < guess:
+        print("Too high!")
+    elif guess == ran_num:
+        print("YOU GOT IT!!")
         want_continue = input("Do you want to keep playing? y/n ")
         if want_continue == "y":
             keep_playing = True
         else:
             keep_playing = False
+    else:
+        print("something went wrong")
