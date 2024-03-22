fc87a0f815814ee3efb2b51d5c219c97f1749ea8
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 18:56:15 -0700

    adjust game to fix issue of new random numbers each time

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ keep_playing = True
 ran_num = random.randint(1, 10)
 guess = None
 
-while guess != ran_num:
+while True:
+    print("random number ",ran_num)
     guess = input("Guess a number between 1 and 10: ")
     guess = int(guess)
-    if guess:
-        if ran_num > guess:
-            print("Too low!")
-        elif ran_num < guess:
-            print("Too high!")
-        elif guess == ran_num:
-            print("YOU GOT IT!!")
-    keep_playing = input("Do you want to keep playing? y/n ")
+    if ran_num > guess:
+        print("Too low!")
+    elif ran_num < guess:
+        print("Too high!")
+    elif guess == ran_num:
+        print("YOU GOT IT!!")
+        keep_playing = input("Do you want to keep playing? y/n ")
     if keep_playing == "y":
         guess = None
+        ran_num = random.randint(1, 10)
 
 
