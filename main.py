734e3c82504e70c412b9d42767d4266103d56191
Denisa Marcisovska <dmarcisovska@gmail.com>
2024-03-21 18:20:12 -0700

    stop copying me guessing game

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+keep_playing = True
+
+while keep_playing:
+    ran_num = random.randint(1, 10)
+    guess = int(input("Guess a number between 1 and 10: "))
+    if guess == ran_num:
+        want_continue = input("Do you want to keep playing? y/n ")
+        if want_continue == "y":
+            keep_playing = True
+        else:
+            keep_playing = False
